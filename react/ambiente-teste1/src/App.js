0d17a373bb6e7ef79cebf78a7ad036a8b7c055c2
Gabriel Soares <116104655+gabriel-hsoares@users.noreply.github.com>
2023-04-13 09:54:13 -0300

    up

---
+++ react/ambiente-teste1/src/App.js
@@ -0,0 +1,12 @@
+import ExibeTecnologia from './pagina';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ExibeTecnologia />
+    </div>
+  );
+}
+
+export default App;
